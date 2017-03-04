58f0c7d947d3c51beaee51cefd7291c239e3f4da
Kuan <j16598231@gmail.com>
2017-03-04 15:12:40 +0800

    line test

--- src/main/java/stock/StockForLINEController.java
+++ src/main/java/stock/StockForLINEController.java
@@ -1,9 +1,14 @@
 package stock;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,6 +24,12 @@ public class StockForLINEController {
 	@ResponseBody
 	public String getStockDetails(@PathVariable("stockNum") String stockNum){
 		return stockForLINEServices.getStockDetails(stockNum);
+	}
+	
+	@PostMapping(value="/Stock")
+	@ResponseBody
+	public void getStockDeatils(@RequestHeader HttpHeaders headers){
+		System.out.println(headers);
 	}
 	
 }
